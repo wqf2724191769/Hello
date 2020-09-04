c8f4ff9019cf016ceda3a2f60053bb570793d6e6
wqf2724191769 <2724191769@qq.com>
2020-09-04 17:49:05 +0800

    first class:HelloWorkd

---
+++ src/com/wqf/HelloWordk.java
@@ -0,0 +1,13 @@
+package com.wqf;
+
+public class HelloWordk {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWordk");
+	}
+
+}
